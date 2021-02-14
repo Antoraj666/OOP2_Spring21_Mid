055593ab5c1fdfa428ae8b181a3b25c66df54f9c
Antoraj666 <78008320+Antoraj666@users.noreply.github.com>
2021-02-14 13:57:32 +0600

    Add files via upload

---
+++ ArrayP3/ArrayP3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int[] arr1 = new int[] { 1, 1, 2, 3, 4 };
+
+            for (int i = 0; i < 5; i++)
+            {
+                count = 0;
+
+                for (int j = 0; j < i - 1; j++)
+                {
+
+                    if (arr1[i] == arr1[j])
+                    {
+                        count++;
+                    }
+                }
+
+                for (int k = i + 1; k < 5; k++)
+                {
+
+                    if (arr1[i] == arr1[k])
+                    {
+                        count++;
+                    }
+
+                    if (arr1[i] == arr1[i + 1])
+                    {
+                        i++;
+                    }
+                }
+
+                if (count == 0)
+                {
+                    Console.Write("{0} ", arr1[i]);
+                }
+            }
+        }
+    }
+}
