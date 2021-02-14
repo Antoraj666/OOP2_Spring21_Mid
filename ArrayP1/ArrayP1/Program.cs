4acaa737469da74bf1e25f577d592dc6476f9f28
Antoraj666 <78008320+Antoraj666@users.noreply.github.com>
2021-02-14 13:23:12 +0600

    Add files via upload

---
+++ ArrayP1/ArrayP1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            int[] arr1 = new int[] { 9, 8, 7, 2, 1 };
+
+            int[] arr2 = new int[arr1.Length];
+
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                arr2[i] = arr1[i];
+            }
+            Console.WriteLine("Elements of array: ");
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                Console.Write(arr1[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arr2.Length; i++)
+            {
+                Console.Write(arr2[i] + " ");
+            }
+        }
+    }
+}
